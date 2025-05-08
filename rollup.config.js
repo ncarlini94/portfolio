c0bc24614188c1217bc9bab536d943a772d0a36f
Nico <ncarlini94@gmail.com>
2025-05-08 16:36:33 -0300

    Change Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,21 @@
 import image from '@rollup/plugin-image';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import react from '@vitejs/plugin-react';
+import css from 'rollup-plugin-css-only';
 
 export default {
-  // ...
+  input: 'src/main.jsx',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    sourcemap: true,
+  },
   plugins: [
+    resolve(),
+    commonjs(),
     image(),
+    css(),
+    react(),
   ]
-}
+};
