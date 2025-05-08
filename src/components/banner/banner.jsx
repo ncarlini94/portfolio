6466a27a504f5ff2428d6e1ce642baf31ee97397
Nico <ncarlini94@gmail.com>
2025-05-08 16:33:20 -0300

    Se agrego Rollup

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -7,7 +7,7 @@ const banner = () => {
     <>
         <div className={Styles.bannerBox}>
             <p className={`${Styles.texto}`}>En búsqueda de oportunidades para aprender y crecer, comprometido con la mejora continua. Listo para asumir nuevos desafíos laborales y contribuir al éxito de los proyectos.</p>
-            <img src={Banner} className={Styles.banner} alt="banner" />
+            <img src={Banner} className={Styles.banner} alt="Banner" />
         </div>
     </>
   )
